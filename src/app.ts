d4b9e94fb5be6146a2cbde06581d7dde08f4cc9b
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2025-04-24 21:20:47 -0300

    feat: add request id middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import { Hono } from 'hono'
 import { pinoLogger } from 'hono-pino'
+import { requestId } from 'hono/request-id'
 
 export const app = new Hono()
 
+app.use(requestId())
 app.use(pinoLogger())
