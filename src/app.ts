df2787a754cdc5355b0492b6005a1556068b422a
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2025-04-25 09:34:48 -0300

    feat: add cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import { Hono } from 'hono'
 import { pinoLogger } from 'hono-pino'
+import { cors } from 'hono/cors'
 import { requestId } from 'hono/request-id'
 
 export const app = new Hono()
 
 app.use(requestId())
 app.use(pinoLogger())
+app.use(cors())
